02c3297705782f34486a6a7e58511f49b5d9c128
baiwusanyu-c <740132583@qq.com>
2024-08-20 17:54:37 +0800

    feat: 新增 tailwindcss 颜色预设

--- packages/tailwindcss/index.ts
+++ packages/tailwindcss/index.ts
@@ -1,3 +1,40 @@
-export function isDemo(){
-  console.log('element-demo')
+import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
+import { genThemeColors } from './colors'
+import { chaPandaPresetShortcut } from './shortcut'
+
+export * from './colors'
+export * from './shortcut'
+
+export const chaPandaTailwindConfig = (
+  userConfig: Config
+) => {
+  const { colors: themeColors } = genThemeColors()
+  // 载入预设的颜色变量
+  if(userConfig.theme && userConfig.theme.colors){
+    const userColors = userConfig.theme!.colors!
+    const isFn = typeof userConfig.theme!.colors === 'function'
+    userConfig.theme.colors = (ctx) => {
+      return {
+        ...themeColors,
+        ...(isFn ? (userColors as Function)(ctx) : userColors),
+      }
+    }
+  } else{
+    userConfig.theme!.colors = ({ colors }) => {
+      return {
+        ...themeColors,
+        ...colors
+      }
+    }
+  }
+  // 载入预设的快捷短语
+  if(userConfig.plugins && Array.isArray(userConfig.plugins)){
+    userConfig.plugins.push(chaPandaPresetShortcut())
+  } else if(!userConfig.plugins ){
+    userConfig.plugins = [
+      plugin(chaPandaPresetShortcut())
+    ]
+  }
+  return userConfig
 }
--- play/tailwindcss/vite.config.ts
+++ play/tailwindcss/vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid()
+  ],
 })
