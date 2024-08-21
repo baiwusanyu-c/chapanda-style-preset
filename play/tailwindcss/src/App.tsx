234b756b183a06f4d37f392c1c8f6dc27cd84d62
baiwusanyu-c <740132583@qq.com>
2024-08-21 13:34:59 +0800

    feat: 新增 tailwindcss 字体DIN预设

--- play/tailwindcss/src/App.tsx
+++ play/tailwindcss/src/App.tsx
@@ -3,10 +3,27 @@ function App() {
   return (
     <>
       <h1 class="
-      font-pingfang
+      test
       bg-cbd-brand-5 fcc border-success">
         Hello world! 7600
       </h1>
+      <p class="pf-regular-24">pf-regular-24 </p>
+      <p class="pf-medium-24"> pf-medium-24 </p>
+      <p class="pf-regular-20">pf-regular-20 </p>
+      <p class="pf-medium-20"> pf-medium-20 </p>
+      <p class="pf-regular-18">pf-regular-18 </p>
+      <p class="pf-medium-18"> pf-medium-18 </p>
+      <p class="pf-regular-17">pf-regular-17 </p>
+      <p class="pf-medium-17"> pf-medium-17 </p>
+      <p class="pf-regular-16">pf-regular-16 </p>
+      <p class="pf-medium-16"> pf-medium-16 </p>
+      <p class="pf-regular-15">pf-regular-15 </p>
+      <p class="pf-medium-15"> pf-medium-15 </p>
+
+      <p class="font-din">din 9999 </p>
+      <p class="font-din-bold"> din-bold 7600</p>
+      <p class="font-din-heavy">din-heavy 9999</p>
+      <p class="font-din-heavy-lighter"> din-heavy-lighter 9999</p>
     </>
   )
 }
--- play/tailwindcss/vite.config.ts
+++ play/tailwindcss/vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import path from "path";
 
 export default defineConfig({
   plugins: [
     solid()
   ],
+  resolve: {
+    alias: {
+      // ✅
+      // TODO 使用 @chapanda/style-preset-tailwind 写入文档
+      '@chapanda/style-preset-base/theme/font': path.resolve(
+        __dirname, 'node_modules/@chapanda/style-preset-tailwind/theme/font'
+      ),
+
+
+
+      // ✅
+      // TODO 使用 @chapanda/style-preset 写入文档
+      // '@chapanda/style-preset-base/theme/font': path.resolve(
+      //   __dirname, 'node_modules/@chapanda/style-preset/theme/font'
+      // ),
+
+      // TODO 打包
+    },
+  },
 })
