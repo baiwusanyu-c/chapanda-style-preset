05ab4085da643b9df0fd6af927189dff4c71daca
baiwusanyu-c <740132583@qq.com>
2024-08-21 17:06:55 +0800

    feat: 新增 unocss、scss、tailwind harmony字体预设

--- packages/unocss/font/index.ts
+++ packages/unocss/font/index.ts
@@ -3,9 +3,11 @@ import type {Theme} from "@unocss/preset-uno";
 
 
 export const chaPandaFontRules = () => {
-  const pfFontFamily = `PingFang-SC, -apple-system, BlinkMacSystemFont, "Segoe UI",
+  const basicFontFamily = `-apple-system, BlinkMacSystemFont, "Segoe UI",
   Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
   "Segoe UI Emoji", "Segoe UI Symbol"`
+  const pfFontFamily = `PingFang-SC, ${basicFontFamily}`
+  const hmFontFamily = `'HarmonyOS Sans SC', ${basicFontFamily}`
   const regular = [24,20,18,17,16,15,14,12,11,10,9]
   const medium = [24,20,18,17,16,15]
   const rules:Rule<Theme>[] = []
@@ -16,6 +18,15 @@ export const chaPandaFontRules = () => {
         'font-weight': '400',
         'font-size': `${p}pt`,
         'font-family': pfFontFamily
+      }
+    ])
+
+    rules.push([
+      `hm-regular-${p}`,
+      {
+        'font-weight': '400',
+        'font-size': `${p}pt`,
+        'font-family': hmFontFamily
       }
     ])
   })
@@ -29,12 +40,28 @@ export const chaPandaFontRules = () => {
         'font-family': pfFontFamily
       }
     ])
+
+    rules.push([
+      `hm-medium-${p}`,
+      {
+        'font-weight': '500',
+        'font-size': `${p}pt`,
+        'font-family': hmFontFamily
+      }
+    ])
   })
 
   rules.push([
     `font-pingfang`,
     {
       'font-family': pfFontFamily
+    }
+  ])
+
+  rules.push([
+    `font-harmony`,
+    {
+      'font-family': hmFontFamily
     }
   ])
 
