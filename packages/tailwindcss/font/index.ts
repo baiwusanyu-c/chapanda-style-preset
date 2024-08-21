6ea7c27f4416441026d9f4c77ad073073ac3720d
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-08-21 17:06:55 +0800

    feat: 新增 unocss、scss、tailwind harmony字体预设

--- packages/tailwindcss/font/index.ts
+++ packages/tailwindcss/font/index.ts
@@ -1,9 +1,12 @@
 
 
 export const chaPandaFontShortcut = () => {
-  const pfFontFamily = `PingFang-SC, -apple-system, BlinkMacSystemFont, "Segoe UI",
+  const basicFontFamily = `-apple-system, BlinkMacSystemFont, "Segoe UI",
   Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
   "Segoe UI Emoji", "Segoe UI Symbol"`
+  const pfFontFamily = `PingFang-SC, ${basicFontFamily}`
+  const hmFontFamily = `'HarmonyOS Sans SC', ${basicFontFamily}`
+
   const regular = [24,20,18,17,16,15,14,12,11,10,9]
   const medium = [24,20,18,17,16,15]
   const shortcut: Record<string, Record<string, string>> = {}
@@ -13,6 +16,11 @@ export const chaPandaFontShortcut = () => {
       'font-size': `${p}pt`,
       'font-family': pfFontFamily
     }
+    shortcut[`.hm-regular-${p}`] = {
+      'font-weight': '400',
+      'font-size': `${p}pt`,
+      'font-family': hmFontFamily
+    }
   })
 
   medium.forEach(p => {
@@ -21,9 +29,17 @@ export const chaPandaFontShortcut = () => {
       'font-size': `${p}pt`,
       'font-family': pfFontFamily
     }
+    shortcut[`.hm-medium-${p}`] = {
+      'font-weight': '500',
+      'font-size': `${p}pt`,
+      'font-family': hmFontFamily
+    }
   })
   shortcut['.font-pingfang'] = {
     'font-family': pfFontFamily
+  }
+  shortcut['.font-harmony'] = {
+    'font-family': hmFontFamily
   }
   return shortcut
 }
