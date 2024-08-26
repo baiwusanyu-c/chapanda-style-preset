735368ff645326b1046c32a1b19cf0b28a7039ce
baiwusanyu-c <740132583@qq.com>
2024-08-26 14:06:52 +0800

    chore: 增加 antd scss 预设

--- play/antd/src/main.tsx
+++ play/antd/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-
+// TODO: 写入文档
+import '@chapanda/style-preset/theme/antd';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
