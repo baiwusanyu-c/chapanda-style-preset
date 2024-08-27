4e0321e910b8b07879805b43cd1bba241c3206c9
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-08-27 17:59:19 +0800

    chore: 删除无用的 vant 预设代码

--- packages/vant/index.ts
+++ packages/vant/index.ts
@@ -2,8 +2,8 @@ import type { ConfigProviderThemeVars  } from "vant";
 import { mergeObjects } from '@chapanda/style-preset-utils'
 import { genVantTheme } from "./theme-config";
 
-export { genButtonToken } from "./theme-config";
 export { genToken } from "./theme-config";
+export { genVantTheme } from "./theme-config";
 export { genThemeColors } from '@chapanda/style-preset-base'
 
 export function genChaPandaVantTheme(theme: ConfigProviderThemeVars = {}){
