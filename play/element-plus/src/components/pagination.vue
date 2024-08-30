164019a748c28044c0eb0dee26d453dbce00aeef
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-08-30 14:36:23 +0800

    chore: 添加 ep 的 pagination 预设

---
+++ play/element-plus/src/components/pagination.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-pagination background layout="prev, pager, next" :total="1000" />
+  <el-pagination background layout="prev, pager, next" :total="1000" disabled/>
+  <div class="example-pagination-block">
+    <div class="example-demonstration">When you have few pages</div>
+    <el-pagination layout="prev, pager, next" :total="50" />
+  </div>
+  <div class="example-pagination-block">
+    <div class="example-demonstration">When you have more than 7 pages</div>
+    <el-pagination layout="prev, pager, next" :total="1000" disabled/>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
