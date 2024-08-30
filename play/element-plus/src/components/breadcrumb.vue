a4dffeac8a1cbb2e24da1d4d769dfb4ef44c0d15
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-08-30 14:11:04 +0800

    chore: 添加 ep 的 breadcrumb 预设

---
+++ play/element-plus/src/components/breadcrumb.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      promotion management
+    </el-breadcrumb-item>
+    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
