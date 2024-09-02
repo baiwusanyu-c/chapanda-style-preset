61938c1a545e650b78d592712d7e957ccfc16632
baiwusanyu-c <740132583@qq.com>
2024-09-02 11:26:43 +0800

    chore: 添加 ep 的 date-picker 预设

--- packages/antd/theme-config/components/date-picker.ts
+++ packages/antd/theme-config/components/date-picker.ts
@@ -1,11 +1,11 @@
 export const genDatePickerToken  = (colors: Record<string, string>) => {
   return {
     "DatePicker": {
-      'activeBg': colors['cbd-gray-3'],
       'cellHoverBg': colors['cbd-gray-3'],
-      'colorTextDisabled': colors['cbd-gray-4'],
-      'colorText': colors['cbd-gray-7'],
       'borderRadiusSM': 50,
+      'colorTextDisabled': colors['cbd-gray-4'],
+      'activeBg': colors['cbd-gray-3'],
+      'colorText': colors['cbd-gray-7'],
     }
   }
 }
