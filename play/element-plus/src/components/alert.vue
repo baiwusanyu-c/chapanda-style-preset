e3d72683ff627a8f3aa1c0c82855bb2b9b6670f4
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-09-04 09:30:05 +0800

    chore: 完成 ep 的 alert 预设

---
+++ play/element-plus/src/components/alert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="max-width: 600px">
+    <el-alert title="Success alert" type="success" show-icon />
+    <el-alert title="Info alert" type="info" show-icon />
+    <el-alert title="Warning alert" type="warning" show-icon />
+    <el-alert title="Error alert" type="error" show-icon />
+  </div>
+</template>
+
+<style scoped>
+.el-alert {
+  margin: 20px 0 0;
+}
+.el-alert:first-child {
+  margin: 0;
+}
+</style>
