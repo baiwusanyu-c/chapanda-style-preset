0a8b58ce6828c9ab49c639cf5d0717aad1510008
baiwusanyu-c <740132583@qq.com>
2025-07-09 10:14:16 +0800

    fix: fix ep disabled button style error

--- play/element-plus/src/components/button.vue
+++ play/element-plus/src/components/button.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+    <el-button >Default</el-button>
+    <el-button  type="primary">Primary</el-button>
+    <el-button  type="success">Success</el-button>
+    <el-button  type="info">Info</el-button>
+    <el-button  type="warning">Warning</el-button>
+    <el-button  type="danger">Danger</el-button>
   </div>
 
   <div class="mb-4">
