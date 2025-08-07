8a59d6f0bc7f0c61fae50f3917950877a3508049
baiwusanyu-c <740132583@qq.com>
2025-08-07 11:42:46 +0800

    fix: fixed the error that the color variable could not be turned off in the base package

--- play/antd/src/main.tsx
+++ play/antd/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 // import '@chapanda/style-preset/theme/antd';
-import '@chapanda/style-preset-antd/theme'
+// import '@chapanda/style-preset-antd/theme'
+import './index.scss'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
